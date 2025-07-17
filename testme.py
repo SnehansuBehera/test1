61d69eaad09c209757b9f5a0b2d192e1800ff571
SnehansuBehera <snehansbehera080903@gmail.com>
2025-07-17 12:21:20 +0530

    deleted-few-lines

--- testme.py
+++ testme.py
@@ -1,12 +1,2 @@
 print("hello")
 print("world")
-print("bye")
-print("hello")  # This line is a duplicate and can be removed
-print("bye")  # This line is a duplicate and can be removed
-# hello --- IGNORE ---
-# --- IGNORE ---
-print("bye")  # This line is a duplicate and can be removed
-
-print("kya hua")
-print("Kuch nahi")
-print("hosh mein ho")
